f2f518f193e3f43be741573b3d30a895da888e7d
Nick Black <dank@qemfd.net>
2010-12-13 22:16:27 -0500

    send packet socket stuff to its own file. fix up makefile for arbitrary sources

---
+++ src/omphalos/psocket.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <omphalos/psocket.h>
+
+// See packet(7) and Documentation/networking/packet_mmap.txt
+int packet_rx_socket(unsigned protocol){
+	struct tpacket_req treq;
+	int fd;
+
+	if((fd = socket(AF_PACKET,SOCK_RAW,htons(protocol))) < 0){
+		fprintf(stderr,"Couldn't open packet socket (%s?)\n",strerror(errno));
+		return -1;
+	}
+	treq.tp_block_size = 0;
+	treq.tp_block_nr = 0;
+	treq.tp_frame_size = 0;
+	treq.tp_frame_nr = 0;
+	return 0;
+}
---
+++ src/omphalos/psocket.h
@@ -0,0 +1,14 @@
+#ifndef OMPHALOS_PSOCKET
+#define OMPHALOS_PSOCKET
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int packet_rx_socket(unsigned protocol);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
