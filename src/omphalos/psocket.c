dfe70e08d1c9be60ef76020055b7e61dab860d95
Nick Black <dank@qemfd.net>
2011-04-06 08:15:47 -0500

    use SOCK_DGRAM rather than _RAW for packet(7) for generic sockaddr_ll

--- src/omphalos/psocket.c
+++ src/omphalos/psocket.c
@@ -15,7 +15,7 @@ static const unsigned MMAP_BLOCK_COUNT = 32768; // FIXME do better
 int packet_socket(unsigned protocol){
 	int fd;
 
-	if((fd = socket(AF_PACKET,SOCK_RAW,htons(protocol))) < 0){
+	if((fd = socket(AF_PACKET,SOCK_DGRAM,htons(protocol))) < 0){
 		fprintf(stderr,"Couldn't open packet socket (%s?)\n",strerror(errno));
 		return -1;
 	}
