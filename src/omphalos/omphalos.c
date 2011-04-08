d2b4298258b0848d6c98dd1fa18fd7f51aa9e3b8
Nick Black <dank@qemfd.net>
2011-04-07 22:17:57 -0500

    set up a signal handler for SIGINT so we can shut down nicely. doing this without either spawning a thread for input packet sockets or requiring a costly memory barrier requires blocking the signal on all othread threads, to ensure it's taken on ours, and using a volatile. gross!

--- src/omphalos/omphalos.c
+++ src/omphalos/omphalos.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/uio.h>
 #include <pthread.h>
 #include <sys/poll.h>
@@ -21,6 +22,18 @@
 
 #define MAXINTERFACES (1u << 16) // lame FIXME
 #define DEFAULT_USERNAME "nobody"
+
+// External cancellation, tested in input-handling loops. This only works
+// without a mutex lock (memory barrier, more precisely) because we
+// restrict signal handling to the input-handling threads (via initial
+// setprocmask() followed by pthread_sigmask() in these threads only). 
+static volatile unsigned cancelled;
+
+static void
+cancellation_signal_handler(int signo __attribute__ ((unused))){
+	cancelled = 1;
+}
+// End nasty signals-based cancellation.
 
 typedef struct omphalos_ctx {
 	const char *pcapfn;
@@ -321,6 +334,7 @@ handle_netlink_event(int fd){
 		// NLMSG_LENGTH sanity checks enforced via NLMSG_OK() and
 		// _NEXT() -- we needn't check amount read within the loop
 		for(nh = (struct nlmsghdr *)buf ; NLMSG_OK(nh,(unsigned)r) ; nh = NLMSG_NEXT(nh,r)){
+			//printf("MSG TYPE %d\n",(int)nh->nlmsg_type);
 			if(nh->nlmsg_flags & NLM_F_MULTI){
 				inmulti = 1;
 			}
@@ -480,15 +494,16 @@ ring_packet_loop(unsigned count,int rfd,void *rxm,const struct tpacket_req *treq
 			handle_ring_packet(rfd,rxm);
 			rxm += inclen(&idx,treq);
 		}
-	}else for( ; ; ){ // FIXME install signal handler
-		handle_ring_packet(rfd,rxm);
-		rxm += inclen(&idx,treq);
+	}else{
+		while(!cancelled){
+			handle_ring_packet(rfd,rxm);
+			rxm += inclen(&idx,treq);
+		}
 	}
 	return 0;
 }
 
 typedef struct netlink_thread_marshal {
-	int fd;
 	char errbuf[256];
 } netlink_thread_marshal;
 
@@ -498,23 +513,35 @@ netlink_thread(void *v){
 	struct pollfd pfd[1] = {
 		{
 			.events = POLLIN | POLLRDNORM | POLLERR,
-			.fd = ntmarsh->fd,
 		}
 	};
 	int events;
 
+	// FIXME how do we ensure this is closed after we get cancelled?
+	if((pfd[0].fd = netlink_socket()) < 0){
+		return NULL;
+	}
+	if(discover_links(pfd[0].fd) < 0){
+		close(pfd[0].fd);
+		return NULL;
+	}
 	strcpy(ntmarsh->errbuf,"");
 	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
 	for(;;){
-		while((events = poll(pfd,sizeof(pfd) / sizeof(*pfd),-1)) == 0){
-			fprintf(stderr,"Interrupted polling netlink socket %d\n",ntmarsh->fd);
+		errno = 0;
+		while((events = poll(pfd,sizeof(pfd) / sizeof(*pfd),-1)) <= 0){
+			fprintf(stderr,"Wakeup on netlink socket %d (%s?)\n",
+					pfd[0].fd,errno ? strerror(errno) : "spontaneous");
+			errno = 0;
+			// FIXME bail on terrible errors?
 		}
 		if(pfd[0].revents & POLLERR){
-			snprintf(ntmarsh->errbuf,sizeof(ntmarsh->errbuf),"Error polling netlink socket %d\n",ntmarsh->fd);
+			snprintf(ntmarsh->errbuf,sizeof(ntmarsh->errbuf),"Error polling netlink socket %d\n",pfd[0].fd);
 			break; // FIXME
 		}else if(pfd[0].revents){
 			handle_netlink_event(pfd[0].fd);
 		}
+		pfd[0].revents = 0;
 	}
 	pthread_exit(ntmarsh->errbuf);
 }
@@ -567,44 +594,71 @@ handle_priv_drop(const char *name){
 	return 0;
 }
 
-static inline int
+static int
+mask_cancel_sigs(sigset_t *oldsigs){
+	struct sigaction sa = {
+		.sa_handler = cancellation_signal_handler,
+		.sa_flags = SA_ONSTACK | SA_RESETHAND | SA_RESTART,
+	};
+	sigset_t cancelsigs;
+
+	if(sigemptyset(&cancelsigs) || sigaddset(&cancelsigs,SIGINT)){
+		fprintf(stderr,"Couldn't prep signals (%s?)\n",strerror(errno));
+		return -1;
+	}
+	if(sigprocmask(SIG_BLOCK,&cancelsigs,oldsigs)){
+		fprintf(stderr,"Couldn't mask signals (%s?)\n",strerror(errno));
+		return -1;
+	}
+	if(sigaction(SIGINT,&sa,NULL)){
+		fprintf(stderr,"Couldn't install sighandler (%s?)\n",strerror(errno));
+		return -1;
+	}
+	return 0;
+}
+
+static int
 handle_packet_socket(const omphalos_ctx *pctx){
 	netlink_thread_marshal ntmarsh;
 	struct tpacket_req rtpr;
-	int rfd,ret = 0;
+	sigset_t oldsigs;
 	pthread_t nltid;
+	int rfd,ret = 0;
 	void *rxm;
 	size_t rs;
-	int nfd;
 
-	// FIXME move into netlink thread
-	if((nfd = netlink_socket()) < 0){
-		return -1;
-	}
-	if(discover_links(nfd) < 0){
-		close(nfd);
-		return -1;
-	}
-	ntmarsh.fd = nfd;
-	if( (errno = pthread_create(&nltid,NULL,netlink_thread,&ntmarsh)) ){
-		fprintf(stderr,"Couldn't create netlink thread (%s?)\n",strerror(errno));
-		close(nfd);
-		return -1;
-	}
 	if((rfd = packet_socket(ETH_P_ALL)) < 0){
-		reap_netlink_thread(nltid);
 		return -1;
 	}
 	if((rs = mmap_rx_psocket(rfd,&rxm,&rtpr)) == 0){
 		close(rfd);
-		reap_netlink_thread(nltid);
 		return -1;
 	}
 	if(handle_priv_drop(pctx->user)){
 		fprintf(stderr,"Couldn't become user %s (%s?)\n",pctx->user,strerror(errno));
 		unmap_psocket(rxm,rs);
 		close(rfd);
+		return -1;
+	}
+	// Before we create other threads, mask cancellation signals. We
+	// only want signals to be handled on the main input threads, so
+	// that we can locklessly test for cancellation.
+	if(mask_cancel_sigs(&oldsigs)){
+		unmap_psocket(rxm,rs);
+		close(rfd);
+		return -1;
+	}
+	if( (errno = pthread_create(&nltid,NULL,netlink_thread,&ntmarsh)) ){
+		fprintf(stderr,"Couldn't create netlink thread (%s?)\n",strerror(errno));
+		unmap_psocket(rxm,rs);
+		close(rfd);
+		return -1;
+	}
+	if(pthread_sigmask(SIG_SETMASK,&oldsigs,NULL)){
+		fprintf(stderr,"Couldn't unmask signals (%s?)\n",strerror(errno));
 		reap_netlink_thread(nltid);
+		unmap_psocket(rxm,rs);
+		close(rfd);
 		return -1;
 	}
 	ret |= ring_packet_loop(pctx->count,rfd,rxm,&rtpr);
@@ -616,7 +670,6 @@ handle_packet_socket(const omphalos_ctx *pctx){
 		ret = -1;
 	}
 	ret |= reap_netlink_thread(nltid);
-	ret |= close(nfd); // FIXME netlink thread, netlink thread!
 	return ret;
 }
 
