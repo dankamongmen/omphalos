a19e9d884076c1df8f229578fa31c2a2f2838fae
Nick Black <dank@qemfd.net>
2011-04-10 07:38:13 -0500

    kill off sll core

--- src/omphalos/sll.c
+++ src/omphalos/sll.c
@@ -1,29 +1,12 @@
 #include <arpa/inet.h>
+#include <omphalos/ip.h>
 #include <omphalos/sll.h>
 #include <linux/if_arp.h>
 #include <asm/byteorder.h>
 #include <linux/if_packet.h>
-#include <omphalos/ethernet.h>
 #include <omphalos/interface.h>
 
 // A cooked packet has had its link-layer header stripped. An l2-neutral
 // struct sockaddr_sll is put in its place.
 void handle_cooked_packet(interface *iface,const unsigned char *pkt,size_t len){
-	struct sockaddr_ll *sll = (struct sockaddr_ll *)pkt;
-
-	if(len < sizeof(*sll)){
-		++iface->malformed;
-		return;
-	}
-	// sll_protocol is in network byte-order. rather than possibly
-	// switch it every time, we provide the cases in network byte-order
-	switch(sll->sll_protocol){
-		case __constant_ntohs(ARPHRD_ETHER):{
-			handle_ethernet_packet(pkt + sizeof(*sll),len - sizeof(*sll));
-			break;
-		}default:{
-			++iface->noprotocol;
-			break;
-		}
-	}
 }
