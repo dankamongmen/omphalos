c83a188f50a0eeba0458714565b2a78d72545150
Nick Black <dank@qemfd.net>
2011-04-13 00:34:39 -0500

    check ARP l2 and l3 length specs against packet length. decode ARP op, to jam ARP requests

--- src/omphalos/arp.c
+++ src/omphalos/arp.c
@@ -10,5 +10,17 @@ void handle_arp_packet(interface *i,const void *frame,size_t len){
 		++i->malformed;
 		return;
 	}
-	// FIXME...
+	if(len != sizeof(*ap) + ap->ar_hln * 2 + ap->ar_pln * 2){
+		fprintf(stderr,"%s malformed expected %zu got %zu\n",
+			__func__, sizeof(*ap) + ap->ar_hln * 2 + ap->ar_pln * 2,len);
+		++i->malformed;
+		return;
+	}
+	switch(ap->ar_op){
+	case ARPOP_REQUEST:{
+		// FIXME reply with ARP spoof...
+	break;}default:{
+		fprintf(stderr,"%s unknown op %u\n",__func__,ap->ar_op);
+		++i->noprotocol;
+	break;}}
 }
