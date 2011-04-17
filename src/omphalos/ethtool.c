42649e260935cc24858458e658755bab5d56ae11
Nick Black <dank@qemfd.net>
2011-04-17 04:52:48 -0500

    add basic ethtool support -- get driver info

---
+++ src/omphalos/ethtool.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/sockios.h>
+#include <linux/ethtool.h>
+#include <omphalos/ethtool.h>
+
+int iface_driver_info(const char *name){
+	struct ethtool_drvinfo drv;
+	struct ifreq ifr;
+	int fd;
+
+	memset(&ifr,0,sizeof(&ifr));
+	if(strlen(name) >= sizeof(ifr.ifr_name)){
+		return -1;
+	}
+	strcpy(ifr.ifr_name,name);
+	drv.cmd = ETHTOOL_GDRVINFO;
+	if((fd = socket(AF_INET,SOCK_DGRAM,0)) < 0){
+		fprintf(stderr,"Couldn't open ethtool fd (%s?)\n",strerror(errno));
+		return -1;
+	}
+	if(ioctl(fd,SIOCETHTOOL,&ifr)){
+		fprintf(stderr,"Couldn't get driver info for %s (%s?)\n",name,strerror(errno));
+		close(fd);
+		return -1;
+	}
+	if(close(fd)){
+		fprintf(stderr,"Couldn't close ethtool fd %d (%s?)\n",fd,strerror(errno));
+		return -1;
+	}
+	return 0;
+}
---
+++ src/omphalos/ethtool.h
@@ -0,0 +1,14 @@
+#ifndef OMPHALOS_ETHTOOL
+#define OMPHALOS_ETHTOOL
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int iface_driver_info(const char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
