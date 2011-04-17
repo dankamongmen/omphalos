1d421890e099750befdd5c36d65dd19f70abbf0f
Nick Black <dank@qemfd.net>
2011-04-17 01:39:37 -0500

    call prctl(PR_SET_KEEPCAPS,1) before setuid(), and add CAP_NET_ADMIN, CAP_NET_RAW back to our CAP_EFFECTIVE set following. fixes bug 142 http://dank.qemfd.net/bugzilla/show_bug.cgi?id=142

--- src/omphalos/privs.c
+++ src/omphalos/privs.c
@@ -1,7 +1,44 @@
 #include <pwd.h>
+#include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/prctl.h>
 #include <sys/capability.h>
+
+static int
+drop_privs(const char *name){
+	const cap_value_t caparray[] = { CAP_NET_ADMIN, CAP_NET_RAW, };
+	struct passwd *pw;
+	cap_t cap;
+
+	// FIXME probably ought use getpwnam_r()...
+	if((pw = getpwnam(name)) == NULL){
+		return -1;
+	}
+	if(prctl(PR_SET_KEEPCAPS,1)){
+		fprintf(stderr,"Couldn't set PR_SET_KEEPCAPS (%s?)\n",strerror(errno));
+		return -1;
+	}
+	if(setuid(pw->pw_uid)){
+		fprintf(stderr,"Couldn't setuid to %u (%s?)\n",pw->pw_uid,strerror(errno));
+		return -1;
+	}
+	if((cap = cap_get_pid(getpid())) == NULL){
+		return -1;
+	}
+	if(cap_set_flag(cap,CAP_EFFECTIVE,sizeof(caparray) / sizeof(*caparray),
+				caparray,CAP_SET)){
+		cap_free(cap);
+		return -1;
+	}
+	if(cap_set_proc(cap)){
+		cap_free(cap);
+		return -1;
+	}
+	cap_free(cap);
+	return 0;
+}
 
 int handle_priv_drop(const char *name){
 	cap_flag_value_t val;
@@ -17,17 +54,13 @@ int handle_priv_drop(const char *name){
 		cap_free(cap);
 		return -1;
 	}
-	cap_free(cap);
 	if(val == CAP_SET){
-		struct passwd *pw = getpwnam(name);
-
-		if(pw == NULL){
-			return -1;
-		}
-		if(setuid(pw->pw_uid)){
+		if(drop_privs(name)){
+			cap_free(cap);
 			return -1;
 		}
 	}
+	cap_free(cap);
 	return 0;
 }
 
