31d4afc2e9e34965071b0b3db20c04f47e4bceae
Nick Black <dank@qemfd.net>
2011-05-01 18:46:07 -0500

    use some color in there. force a black background to be painted for now...we might try working with white terminals later, maybe

--- src/ui/ncurses.c
+++ src/ui/ncurses.c
@@ -8,12 +8,38 @@
 #define PROGNAME "omphalos"	// FIXME
 #define VERSION  "0.98pre"	// FIXME
 
+enum {
+	BORDER_COLOR = 1,
+	HEADING_COLOR = 2,
+};
+
 static int
 draw_main_window(WINDOW *w,const char *name,const char *ver){
+	if(wcolor_set(w,BORDER_COLOR,NULL) != OK){
+		return -1;
+	}
 	if(box(w,0,0) != OK){
 		return -1;
 	}
-	if(mvwprintw(w,0,2,"[%s %s]",name,ver) < 0){
+	if(mvwprintw(w,0,2,"[") < 0){
+		return -1;
+	}
+	if(wcolor_set(w,HEADING_COLOR,NULL) != OK){
+		return -1;
+	}
+	if(wattron(w,A_BOLD) != OK){
+		return -1;
+	}
+	if(wprintw(w,"%s %s",name,ver) < 0){
+		return -1;
+	}
+	if(wattroff(w,A_BOLD) != OK){
+		return -1;
+	}
+	if(wcolor_set(w,BORDER_COLOR,NULL) != OK){
+		return -1;
+	}
+	if(wprintw(w,"]") < 0){
 		return -1;
 	}
 	if(wrefresh(w)){
@@ -25,12 +51,50 @@ draw_main_window(WINDOW *w,const char *name,const char *ver){
 // Cleanup which ought be performed even if we had a failure elsewhere, or
 // indeed never started.
 static int
-mandatory_cleanup(void){
-	if(endwin() != OK){
-		fprintf(stderr,"Couldn't cleanup ncurses\n");
-		return -1;
+mandatory_cleanup(WINDOW *w){
+	int ret = 0;
+
+	if(delwin(w) != OK){
+		ret = -1;
 	}
-	return 0;
+	if(endwin() != OK){
+		ret = -1;
+	}
+	if(ret){
+		fprintf(stderr,"Couldn't cleanup ncurses\n");
+	}
+	return ret;
+}
+
+static WINDOW *
+ncurses_setup(void){
+	WINDOW *w;
+
+	if((w = initscr()) == NULL){
+		fprintf(stderr,"Couldn't initialize ncurses\n");
+		goto err;
+	}
+	if(start_color() != OK){
+		fprintf(stderr,"Couldn't initialize ncurses color\n");
+		goto err;
+	}
+	if(use_default_colors()){
+		fprintf(stderr,"Couldn't initialize ncurses colordefs\n");
+		goto err;
+	}
+	if(init_pair(BORDER_COLOR,COLOR_GREEN,COLOR_BLACK)){
+		fprintf(stderr,"Couldn't initialize ncurses colorpair\n");
+		goto err;
+	}
+	if(init_pair(HEADING_COLOR,COLOR_YELLOW,COLOR_BLACK)){
+		fprintf(stderr,"Couldn't initialize ncurses colorpair\n");
+		goto err;
+	}
+	return w;
+
+err:
+	mandatory_cleanup(w);
+	return NULL;
 }
 
 int main(int argc,char * const *argv){
@@ -40,13 +104,8 @@ int main(int argc,char * const *argv){
 		fprintf(stderr,"Couldn't initialize locale (%s?)\n",strerror(errno));
 		return EXIT_FAILURE;
 	}
-	if((w = initscr()) == NULL){
-		fprintf(stderr,"Couldn't initialize ncurses\n");
-		goto err;
-	}
-	if(start_color() != OK){
-		fprintf(stderr,"Couldn't initialize ncurses color\n");
-		goto err;
+	if((w = ncurses_setup()) == NULL){
+		return EXIT_FAILURE;
 	}
 	if(draw_main_window(w,PROGNAME,VERSION)){
 		fprintf(stderr,"Couldn't use ncurses\n");
@@ -56,12 +115,12 @@ int main(int argc,char * const *argv){
 		goto err;
 	}
 	omphalos_cleanup();
-	if(mandatory_cleanup()){
+	if(mandatory_cleanup(w)){
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 
 err:
-	mandatory_cleanup();
+	mandatory_cleanup(w);
 	return EXIT_FAILURE;
 }
