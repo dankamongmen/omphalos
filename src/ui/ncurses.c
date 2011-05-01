b1ae7241023eefd24164c60f99eb1d6ea688b67a
Nick Black <dank@qemfd.net>
2011-05-01 18:11:17 -0500

    draw the initial ui border. always call endwin() for ncurses cleanup

--- src/ui/ncurses.c
+++ src/ui/ncurses.c
@@ -5,6 +5,33 @@
 #include <ncurses.h>
 #include <omphalos/omphalos.h>
 
+#define PROGNAME "omphalos" // FIXME
+
+static int
+draw_main_window(WINDOW *w,const char *name){
+	if(wprintw(w,"%s\n",name) < 0){
+		return -1;
+	}
+	if(box(w,0,0) != OK){
+		return -1;
+	}
+	if(wrefresh(w)){
+		return -1;
+	}
+	return 0;
+}
+
+// Cleanup which ought be performed even if we had a failure elsewhere, or
+// indeed never started.
+static int
+mandatory_cleanup(void){
+	if(endwin() != OK){
+		fprintf(stderr,"Couldn't cleanup ncurses\n");
+		return -1;
+	}
+	return 0;
+}
+
 int main(int argc,char * const *argv){
 	WINDOW *w;
 
@@ -14,16 +41,22 @@ int main(int argc,char * const *argv){
 	}
 	if((w = initscr()) == NULL){
 		fprintf(stderr,"Couldn't initialize ncurses\n");
-		return EXIT_FAILURE;
+		goto err;
 	}
-	refresh();
+	if(draw_main_window(w,PROGNAME)){
+		fprintf(stderr,"Couldn't use ncurses\n");
+		goto err;
+	}
 	if(omphalos_init(argc,argv)){
-		return EXIT_FAILURE;
+		goto err;
 	}
 	omphalos_cleanup();
-	if(endwin() != OK){
-		fprintf(stderr,"Couldn't cleanup ncurses\n");
+	if(mandatory_cleanup()){
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
+
+err:
+	mandatory_cleanup();
+	return EXIT_FAILURE;
 }
