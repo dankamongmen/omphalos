15dfc8123b1e5bdaca036731bb5df7fd4018a44f
Nick Black <dank@qemfd.net>
2011-05-02 23:41:29 -0500

    start in with the ui callback initerface

--- src/omphalos/omphalos.h
+++ src/omphalos/omphalos.h
@@ -5,16 +5,32 @@
 extern "C" {
 #endif
 
+struct interface;
+
 // Process-scope settings, generally configured on startup based off
 // command-line options.
 typedef struct omphalos_ctx {
+	// PCAP-format filename FIXME support multiple?
 	const char *pcapfn;
+	// Exit after processing this many packets. 0 for all packets in
+	// the file, or to run until interrupted for packet sockets.
 	unsigned long count;
+
+	// Username to setuid() to.
 	const char *user;
+
+	// UI callback interface. Any number may be NULL.
+	void (*iface_event)(const struct interface *);
 } omphalos_ctx;
 
+// Parse the command line for common arguments (a UI introducing its own
+// CLI arguments would need to extract them before calling, as stands
+// FIXME). Initializes and prepares an omphalos_ctx.
 int omphalos_setup(int,char * const *,omphalos_ctx *);
+
+// Run the provided context
 int omphalos_init(const omphalos_ctx *);
+
 void omphalos_cleanup(void);
 
 #ifdef __cplusplus
