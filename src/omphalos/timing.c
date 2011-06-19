1db4b5649892faf00a6cb69488500386942772f8
nick black <dankmongmen@gmail.com>
2011-06-18 20:41:18 -0500

    implement a ringbuffer-backed statistic over a time domain

--- src/omphalos/timing.c
+++ src/omphalos/timing.c
@@ -2,24 +2,79 @@
 #include <stdlib.h>
 #include <omphalos/timing.h>
 
-int timestat_prep(timestat *ts,unsigned persec,unsigned total){
+int timestat_prep(timestat *ts,unsigned usec,unsigned total){
 	if((ts->counts = malloc(sizeof(*ts->counts) * total)) == NULL){
 		return -1;
 	}
 	memset(ts->counts,0,sizeof(*ts->counts) * total);
 	gettimeofday(&ts->firstsamp,NULL);
 	ts->firstidx = 0;
-	ts->persec = persec;
 	ts->total = total;
+	ts->usec = usec;
 	return 0;
+}
+
+static inline unsigned
+ringinc(unsigned idx,unsigned move,unsigned s){
+	return idx + move % s;
 }
 
 void timestat_inc(timestat *ts,const struct timeval *tv){
 	struct timeval diff;
 	unsigned long usec;
+	unsigned distance;
 
 	timersub(tv,&ts->firstsamp,&diff);
        	usec = timerusec(&diff);
+	// Get the number of samples between us and the first sample
+	distance = usec / ts->usec;
+	// This is equivalent to moving forward a slot, since we can't move
+	// forward without expiring some count once we've filled the ringbuf
+	// once. Before we've filled the ring once, we can move forward without
+	// expiry, but at that time the ring is initialized to 0's anyway, so
+	// this is the only time (other than init) that we need to zero counts.
+	// We zero out min(expired,ts->total), which is always at least 1, so
+	// we always zero our own new slot. There's thus no need to track a
+	// last sample time; the first tracked sample time is sufficient.
+	if(distance >= ts->total){
+		struct timeval adv;
+		unsigned expired;
+
+		// Some counts have expired (if the distance is greater than or
+		// equal to twice the total, all of them have expired). First,
+		// determine how many to keep...
+		expired = distance - ts->total + 1; // this many expired
+		if(expired < ts->total){ // keep some
+			unsigned idx; // new slot's idx
+
+			idx = ringinc(ts->firstidx,distance,ts->total);
+			ts->firstidx = ringinc(ts->firstidx,expired,ts->total);
+			distance -= expired;
+			// zero out our new slot and any that we've skipped
+			// over (expired in total). might be disjoint.
+			if(idx + 1 < expired){
+				unsigned rexpir = expired - (idx + 1);
+
+				memset(ts->counts,0,
+					sizeof(*ts->counts) * (idx + 1));
+				memset(ts->counts + (ts->total - rexpir),0,
+						sizeof(*ts->counts) * rexpir);
+			}else{ // we can get all expired counts with one memset
+				memset(ts->counts + ((idx + 1) - expired),0,
+					sizeof(*ts->counts) * expired);
+			}
+		}else{ // lose all; start over at head of ring, zero out all
+			ts->firstidx = 0;
+			distance = 0;
+			memset(ts->counts,0,sizeof(*ts->counts) * ts->total);
+		}
+		// Base the time off distance * ts->usec + firstsamp,
+		// normalizing time of the sample within the period.
+		adv.tv_sec = ((distance - ts->total) * ts->usec) / 1000000;
+		adv.tv_usec = ((distance - ts->total) * ts->usec) % 1000000;
+		timeradd(&ts->firstsamp,&adv,&ts->firstsamp);
+	}
+	++ts->counts[ringinc(ts->firstidx,distance,ts->total)]; // inc counter
 }
 
 void timestat_destroy(timestat *ts){
--- src/omphalos/timing.h
+++ src/omphalos/timing.h
@@ -8,18 +8,20 @@ extern "C" {
 // We want to support finite time-sliced statistics, to for instance show the
 // bitrate on an interface for the last 5s at 50Hz sampling. Sampling at a
 // higher rate than the video sync isn't useful for UI's, but might be
-// desirable for headless drivers.
+// desirable for headless drivers. We operate at microsecond resolution, though
+// might not get that much from gettimeofday().
 
 #include <stdint.h>
 #include <sys/time.h>
 
 typedef struct timestat {
 	uint32_t *counts;		// (count = rate * time)-entry ringbuf
-	unsigned persec,total;		// counts per sec, number of counts
+	unsigned usec,total;		// usec per count, number of counts
 	unsigned firstidx;		// index of first sample in ringbuffer
 	struct timeval firstsamp;	// time associated with first sample
 } timestat;
 
+// For 5s at 50Hz, provide 20000 and 250 -- 50Hz means 20ms per counter.
 int timestat_prep(timestat *,unsigned,unsigned);
 void timestat_inc(timestat *,const struct timeval *tv);
 void timestat_destroy(timestat *);
