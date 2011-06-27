29bde3c19c660ce50f28b70b8a108e30c9d68d0f
nick black <dank@qemfd.net>
2011-06-27 18:42:25 -0500

    stop using subwindows, and just use new windows for interfaces. this fixes our trails, and is how we ought have been doing things all along

--- src/ui/ncurses/ncurses.c
+++ src/ui/ncurses/ncurses.c
@@ -1387,7 +1387,7 @@ interface_cb_locked(const interface *i,int inum,iface_state *ret){
 					ret->next->prev = ret;
 					ret->prev->next = ret;
 				}
-				if( (ret->subwin = derwin(pad,ret->ysize,PAD_COLS,ret->scrline,START_COL)) &&
+				if( (ret->subwin = newwin(ret->ysize,PAD_COLS,ret->scrline,START_COL)) &&
 						(ret->panel = new_panel(ret->subwin)) ){
 					// Want the subdisplay left above this
 					// new iface, should they intersect.
