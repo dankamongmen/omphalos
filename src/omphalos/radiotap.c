5e7ae6f6caf38dafde177ef8d0e777542803e485
Nick Black <dank@qemfd.net>
2011-06-28 13:13:14 -0500

    no newline in radiotap status message

--- src/omphalos/radiotap.c
+++ src/omphalos/radiotap.c
@@ -5,7 +5,7 @@
 
 void handle_radiotap_packet(const omphalos_iface *octx,omphalos_packet *op,
 				const void *frame,size_t len){
-	octx->diagnostic("FIXME radiotap %p/%zu (%s)\n",frame,len,op->i->name);
+	octx->diagnostic("FIXME radiotap %p/%zu (%s)",frame,len,op->i->name);
 	if(octx->packet_read){
 		octx->packet_read(op->i->opaque,op);
 	}
