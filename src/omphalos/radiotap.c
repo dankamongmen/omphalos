2caec361910c86553f650b94cbbb53c0804f51c8
Nick Black <dank@qemfd.net>
2011-08-08 17:37:10 -0500

    grmmm

--- src/omphalos/radiotap.c
+++ src/omphalos/radiotap.c
@@ -15,7 +15,7 @@ typedef struct ieee80211hdr {
 	uint16_t control;
 	uint16_t duration;
 	unsigned char h_dest[ETH_ALEN];
-	unsigned char h_src[ETH_ALEN];
+	// FIXME unsigned char h_src[ETH_ALEN]; see below
 } __attribute__ ((packed)) ieee80211hdr;
 
 static void
@@ -24,13 +24,15 @@ handle_ieee80211_packet(const omphalos_iface *octx,omphalos_packet *op,
 	const ieee80211hdr *ihdr = frame;
 	struct l2host *l2s;
 
+	// FIXME certain packets don't have the full 802.11 header (8 bytes,
+	// control/duration/h_dest, seems to be the minimum).
 	if(len < sizeof(ieee80211hdr)){
 		++op->i->malformed;
 		octx->diagnostic("%s Packet too small (%zu) on %s",
 				__func__,len,op->i->name);
 		return;
 	}
-	l2s = lookup_l2host(octx,op->i,ihdr->h_src);
+	l2s = lookup_l2host(octx,op->i,ihdr->h_dest);
 	op->l2s = l2s;
 }
 
