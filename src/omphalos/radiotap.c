bbc6cfef1e4f086a3d3345b20d626a8a1942cdd6
nick black <dank@qemfd.net>
2011-08-11 04:58:55 -0500

    fix monitor mode, yee-haw!

--- src/omphalos/radiotap.c
+++ src/omphalos/radiotap.c
@@ -8,7 +8,10 @@
 // FIXME i guess we need a copy of the upstream, since linux doesn't
 // seem to install it? dubious, very dubious...
 typedef struct radiotaphdr {
-	char data[26];			// wants 26 on pure monitor mode FIXME
+	uint8_t version;
+	uint8_t pad;
+	uint16_t len;
+	uint32_t present;
 } __attribute__ ((packed)) radiotaphdr;
 
 typedef struct ieee80211hdr {
@@ -38,15 +41,32 @@ handle_ieee80211_packet(const omphalos_iface *octx,omphalos_packet *op,
 
 void handle_radiotap_packet(const omphalos_iface *octx,omphalos_packet *op,
 				const void *frame,size_t len){
-	const char *sframe;
+	const radiotaphdr *rhdr = frame;
+	const void *ehdr;
+	unsigned rlen;
 
+	// FIXME certain packets don't have the full 802.11 header (8 bytes,
+	// control/duration/h_dest, seems to be the minimum).
 	if(len < sizeof(radiotaphdr)){
 		++op->i->malformed;
 		octx->diagnostic("%s Packet too small (%zu) on %s",
 				__func__,len,op->i->name);
 		return;
 	}
-	sframe = (const char *)frame + sizeof(radiotaphdr);
-	len -= sizeof(radiotaphdr);
-	handle_ieee80211_packet(octx,op,sframe,len);
+	if(rhdr->version != 0){
+		++op->i->noprotocol;
+		octx->diagnostic("%s Unknown radiotap version %zu on %s",
+				__func__,rhdr->version,op->i->name);
+		return;
+	}
+	rlen = rhdr->len;
+	if(len < rlen){
+		++op->i->malformed;
+		octx->diagnostic("%s Radiotap too small (%zu < %zu) on %s",
+				__func__,len,rlen,op->i->name);
+		return;
+	}
+	ehdr = (const char *)frame + rlen;
+	len -= rlen;
+	handle_ieee80211_packet(octx,op,ehdr,len);
 }
