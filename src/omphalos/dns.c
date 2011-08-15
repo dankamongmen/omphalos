4cf9261c7ac7ba3114796c3d5b85f56ef26aef10
nick black <dank@qemfd.net>
2011-08-15 00:23:12 -0500

    dns PTR extraction for ipv4

--- src/omphalos/dns.c
+++ src/omphalos/dns.c
@@ -1,7 +1,15 @@
+#include <ctype.h>
+#include <assert.h>
 #include <stdint.h>
 #include <omphalos/dns.h>
+#include <asm/byteorder.h>
 #include <omphalos/omphalos.h>
 #include <omphalos/interface.h>
+
+#define DNS_CLASS_IN	__constant_ntohs(1u)
+#define DNS_TYPE_A	__constant_ntohs(1u)
+#define DNS_TYPE_PTR	__constant_ntohs(12u)
+#define DNS_TYPE_AAAA	__constant_ntohs(28u)
 
 struct dnshdr {
 	uint16_t id;
@@ -9,6 +17,96 @@ struct dnshdr {
 	uint16_t qdcount,ancount,nscount,arcount;
 	// question, answer, authority, and additional sections follow
 };
+
+// FIXME is it safe to be using (possibly signed) naked chars?
+// FIXME handle AAAA lookups (".ip6.arpa")!
+static int
+process_reverse_lookup(const char *buf,size_t len,int *fam,struct sockaddr_storage *addr){
+	char obuf[INET6_ADDRSTRLEN];
+	const char *chunk,*c;
+	unsigned quad;
+	char q[4][5];
+
+	if(len < __builtin_strlen(".in-addr.arpa")){
+		return -1;
+	}
+	const size_t xlen = len - __builtin_strlen(".in-addr.arpa");
+	if(strcmp(buf + xlen - 1,".in-addr.arpa")){
+		return -1;
+	}
+	// Don't need to worry about checks against len, since we'll hit 'i'
+	// from 'in-addr.arpa' and exit.
+	chunk = buf;
+	for(quad = 0 ; quad < 4 ; ++quad){
+		for(c = chunk ; c - chunk < 3 ; ++c){
+			if(isdigit(*c)){
+				q[quad][c - chunk] = *c;
+			}else if(*c == '.'){
+				break;
+			}else{
+				return -1;
+			}
+		}
+		if(c - buf == 3){
+			return -1;
+		}
+		q[quad][c - chunk] = '.';
+		q[quad][c - chunk + 1] = '\0';
+		chunk = c + 1;
+	}
+	*obuf = '\0';
+	while(quad--){
+		strcat(obuf,q[quad]);
+	}
+	obuf[xlen - 1] = '\0';
+	if(inet_pton(AF_INET,obuf,addr) != 1){
+		return -1;
+	}
+	*fam = AF_INET;
+	return 0;
+}
+
+static char *
+extract_dns_record(size_t len,const unsigned char *sec,unsigned *class,
+			unsigned *type,unsigned *bsize){
+	unsigned char rlen,*buf = NULL;
+	unsigned idx = 0;
+
+	*bsize = 0;
+	while(len > ++idx && (rlen = *sec++)){
+		const unsigned char *tmp;
+
+		if(rlen >= 192 || rlen + idx > len){
+			free(buf);
+			goto malformed;
+		}
+		// If there was any previous length, it was
+		// nul-terminated. We will be writing over said nul with
+		// a '.', so count all that length. We'll then need the
+		// new characters (rlen), and a nul term.
+		if((tmp = realloc(buf,bsize + rlen + 1)) == NULL){
+			free(buf);
+			goto malformed;
+		}
+		buf = tmp;
+		if(bsize){
+			buf[bsize - 1] = '.';
+		}
+		strncpy(buf + bsize,(const char *)sec,rlen);
+		bsize += rlen + 1;
+		buf[bsize - 1] = '\0';
+		sec += rlen;
+		idx += rlen;
+	}
+	if(len < idx + 4 || buf == NULL){
+		free(buf);
+		goto malformed;
+	}
+	*class = *((uint16_t *)sec + 1)
+	*type = *(uint16_t *)sec;
+	*bsize += 4;
+	return buf;
+}
 
 void handle_dns_packet(const omphalos_iface *octx,omphalos_packet *op,const void *frame,size_t len){
 	const struct dnshdr *dns = frame;
@@ -23,6 +121,7 @@ void handle_dns_packet(const omphalos_iface *octx,omphalos_packet *op,const void
 	}
 	//opcode = (ntohs(dns->flags) & 0x7800) >> 11u;
 	qd = ntohs(dns->qdcount);
+	assert(qd == 1);
 	an = ntohs(dns->ancount);
 	ns = ntohs(dns->nscount);
 	ar = ntohs(dns->arcount);
@@ -30,43 +129,28 @@ void handle_dns_packet(const omphalos_iface *octx,omphalos_packet *op,const void
 	sec = (const unsigned char *)frame + sizeof(*dns);
 	//octx->diagnostic("q/a/n/a: %hu/%hu/%hu/%hu",qd,an,ns,ar);
 	if(qd){
-		char *tmp,*buf = NULL;
-		unsigned char rlen;
-		unsigned idx = 0;
-		size_t bsize = 0;
+		size_t bsize;
 
-		while(len > ++idx && (rlen = *sec++)){
-			if(rlen >= 192 || rlen + idx > len){
-				free(buf);
-				goto malformed;
-			}
-			// If there was any previous length, it was
-			// nul-terminated. We will be writing over said nul with
-			// a '.', so count all that length. We'll then need the
-			// new characters (rlen), and a nul term.
-			if((tmp = realloc(buf,bsize + rlen + 1)) == NULL){
-				free(buf);
-				goto malformed;
-			}
-			buf = tmp;
-			if(bsize){
-				buf[bsize - 1] = '.';
-			}
-			strncpy(buf + bsize,(const char *)sec,rlen);
-			bsize += rlen + 1;
-			buf[bsize - 1] = '\0';
-			sec += rlen;
-			idx += rlen;
-		}
-		if(len < idx + 4 || buf == NULL){
-			free(buf);
+		buf = extract_dns_record(sec,len,&class,&type,&bsize);
+		if(buf == NULL){
 			goto malformed;
 		}
-		//octx->diagnostic("TYPE: %hu CLASS: %hu",
-		//		ntohs(*(uint16_t *)sec),ntohs(*((uint16_t *)sec + 1)));
-		//octx->diagnostic("QUESTION: %s",buf);
-		// FIXME
+		if(class == DNS_CLASS_IN){
+			if(rr == DNS_TYPE_PTR){
+				struct sockaddr_storage ss;
+				int fam;
+
+				if(process_reverse_lookup(buf,bsize,&fam,&ss)){
+					goto malformed;
+				}
+			}
+			//octx->diagnostic("TYPE: %hu CLASS: %hu",
+			//		,ntohs(*((uint16_t *)sec + 1)));
+		}
+		// FIXME handle A/AAAA
 		free(buf);
+		sec += bsize;
+		len -= bsize;
 	}
 	if(an){
 		if(len == 0){
