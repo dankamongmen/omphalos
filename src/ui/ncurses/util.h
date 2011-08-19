d6d34d73261efa09e64021be398d1c34c2d11c4a
nick black <dank@qemfd.net>
2011-08-19 03:55:30 -0500

    drop A_BOLD kill

--- src/ui/ncurses/util.h
+++ src/ui/ncurses/util.h
@@ -39,10 +39,22 @@ enum {
 	PHEADING_COLOR,
 	BULKTEXT_COLOR,			// bulk text (help, details)
 	IFACE_COLOR,			// interface summary text
+	LCAST_COLOR,			// local addresses
+	UCAST_COLOR,			// unicast addresses
 	MCAST_COLOR,			// multicast addresses
 	BCAST_COLOR,			// broadcast addresses
 	ROUTER_COLOR,			// routing l3 addresses / access points
 	MAX_OMPHALOS_COLOR
+};
+
+// FIXME on 8-color terminals (linux, xterm), we can't define more than
+// COLOR_WHITE + 1 colors at once. we'll need use A_BOLD there, which is not a
+// problem since we can't fade on those terminals anyway (can_color_change()
+// returns FALSE).
+enum {
+	COLOR_BOLDCYAN  = COLOR_WHITE + 1,
+	COLOR_BOLDBLUE,
+	COLOR_MAX
 };
 
 void fade(unsigned);
