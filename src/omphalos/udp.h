96d3f3d80d6c675e2ab14b18160be8807a269226
Nick Black <dank@qemfd.net>
2011-08-28 17:02:09 -0500

    export udp4_csum()

--- src/omphalos/udp.h
+++ src/omphalos/udp.h
@@ -6,12 +6,15 @@ extern "C" {
 #endif
 
 #include <stddef.h>
+#include <stdint.h>
 
 struct omphalos_iface;
 struct omphalos_packet;
 
 void handle_udp_packet(const struct omphalos_iface *,struct omphalos_packet *,
 					const void *,size_t);
+
+uint16_t udp4_csum(const void *hdr) __attribute__ ((nonnull (1)));
 
 #ifdef __cplusplus
 }
