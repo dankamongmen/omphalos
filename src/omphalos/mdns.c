9413ac77645d5d45ca39f3027dba08d231887aff
nick black <dank@qemfd.net>
2011-10-11 20:06:26 -0400

    add tx_mdns_ptr() for transmitting mdns lookups

--- src/omphalos/mdns.c
+++ src/omphalos/mdns.c
@@ -1,9 +1,32 @@
 #include <assert.h>
+#include <sys/socket.h>
+#include <omphalos/tx.h>
 #include <omphalos/dns.h>
 #include <omphalos/mdns.h>
+#include <omphalos/route.h>
 #include <omphalos/omphalos.h>
+#include <omphalos/interface.h>
 
 void handle_mdns_packet(const omphalos_iface *iface,omphalos_packet *op,
 			const void *frame,size_t len){
 	handle_dns_packet(iface,op,frame,len);
+}
+
+int tx_mdns_ptr(const omphalos_iface *octx,interface *i,int fam,const char *str){
+	void *mcast_addr = NULL;
+	struct routepath rp;
+	size_t flen;
+	void *frame;
+
+	assert(fam == AF_INET || fam == AF_INET6);
+	if(!(i->flags & IFF_MULTICAST)){
+		return 0;
+	}
+	frame = get_tx_frame(octx,i,&flen);
+	if(setup_dns_ptr(&rp,fam,flen,mcast_addr,frame,str)){
+		abort_tx_frame(octx,i,frame);
+		return -1;
+	}
+	send_tx_frame(octx,i,frame);
+	return 0;
 }
--- src/omphalos/mdns.h
+++ src/omphalos/mdns.h
@@ -7,11 +7,17 @@ extern "C" {
 
 #include <stddef.h>
 
+struct l2host;
+struct l3host;
+struct interface;
 struct omphalos_iface;
 struct omphalos_packet;
 
 void handle_mdns_packet(const struct omphalos_iface *,struct omphalos_packet *,
 			const void *,size_t) __attribute__ ((nonnull (1,2,3)));
+
+int tx_mdns_ptr(const struct omphalos_iface *,struct interface *,int,
+		const char *) __attribute__ ((nonnull (1,2,4)));
 
 #ifdef __cplusplus
 }
