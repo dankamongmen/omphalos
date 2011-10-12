d76e4901d1dfcd64ca560c86084ac60c02bb31da
nick black <dank@qemfd.net>
2011-10-12 14:15:02 -0400

    need lock in rtm_newneigh(). resolves one part of bug 258

--- src/omphalos/netlink.c
+++ src/omphalos/netlink.c
@@ -184,8 +184,10 @@ handle_rtm_newneigh(const omphalos_iface *octx,const struct nlmsghdr *nl){
 		if(!(nd->ndm_state & (NUD_NOARP|NUD_FAILED|NUD_INCOMPLETE))){
 			struct l2host *l2;
 
+			lock_interface(iface);
 			l2 = lookup_l2host(octx,iface,ll);
 			lookup_l3host(octx,iface,l2,nd->ndm_family,ad);
+			unlock_interface(iface);
 		}
 	}
 	return 0;
