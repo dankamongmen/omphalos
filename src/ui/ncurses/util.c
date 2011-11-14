9514079bdf1ca45a771f154ed92fbcb5e1b897cb
nick black <dank@qemfd.net>
2011-11-14 17:49:34 -0500

    exclusively wide-char output for beveled boxes

--- src/ui/ncurses/util.c
+++ src/ui/ncurses/util.c
@@ -49,14 +49,17 @@ int bevel(WINDOW *w){
 	int rows,cols;
 
 	getmaxyx(w,rows,cols);
-	assert(box(w,0,0) != ERR);
 	// called as one expects: 'mvwadd_wch(w,rows - 1,cols - 1,&bchr[3]);'
 	// we get ERR returned. this is known behavior: fuck ncurses. instead,
 	// we use mvwins_wch, which doesn't update the cursor position.
 	// see http://lists.gnu.org/archive/html/bug-ncurses/2007-09/msg00001.html
 	assert(mvwadd_wch(w,0,0,&bchr[0]) != ERR);
+	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
 	assert(mvwins_wch(w,0,cols - 1,&bchr[1]) != ERR);
+	assert(mvwvline(w,1,cols - 1,ACS_VLINE,rows - 1) != ERR);
+	assert(mvwvline(w,1,0,ACS_VLINE,rows - 1) != ERR);
 	assert(mvwadd_wch(w,rows - 1,0,&bchr[2]) != ERR);
+	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
 	assert(mvwins_wch(w,rows - 1,cols - 1,&bchr[3]) != ERR);
 	return OK;
 }
