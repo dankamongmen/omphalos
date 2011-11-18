7f149f61da0aabaea7235623cc1335932e932225
Nick Black <dank@qemfd.net>
2011-11-18 08:50:05 -0500

    add skeleton for protocol-agnostic tx_broadcast_pings() (see bug 285)

--- src/omphalos/icmp.c
+++ src/omphalos/icmp.c
@@ -1,4 +1,5 @@
 #include <linux/icmp.h>
+#include <omphalos/tx.h>
 #include <omphalos/icmp.h>
 #include <omphalos/diag.h>
 #include <omphalos/omphalos.h>
@@ -24,4 +25,40 @@ void handle_icmp6_packet(omphalos_packet *op,const void *frame,size_t len){
 		return;
 	}
 	// FIXME
+}
+
+// Always goes to ff02::2 (ALL-HOSTS), from each source address.
+static int
+tx_ipv4_bcast_pings(interface *i){
+	assert(i); // FIXME
+	return -1;
+}
+
+// Always goes to ff02::2 (ALL-HOSTS), from each source address.
+static int
+tx_ipv6_bcast_pings(interface *i){
+	const struct ip6route *i6;
+	int ret = 0;
+
+	for(i6 = i->ip6r ; i6 ; i6 = i6->next){
+		size_t flen;
+		void *frame;
+
+		if((frame = get_tx_frame(i,&flen)) == NULL){
+			ret = -1;
+			continue;
+		}
+		// FIXME prepare ipv6 broadcast
+		send_tx_frame(i,frame); // FIXME get return value...
+	}
+	return ret;
+}
+
+int tx_broadcast_pings(int fam,interface *i){
+	if(fam == AF_INET){
+		return tx_ipv4_bcast_pings(i);
+	}else if(fam == AF_INET6){
+		return tx_ipv6_bcast_pings(i);
+	}
+	return -1;
 }
--- src/omphalos/icmp.h
+++ src/omphalos/icmp.h
@@ -11,11 +11,14 @@ extern "C" {
 #define IPPROTO_ICMP6	58
 #endif
 
+struct interface;
 struct omphalos_packet;
 
 void handle_icmp_packet(struct omphalos_packet *,const void *,size_t);
 
 void handle_icmp6_packet(struct omphalos_packet *,const void *,size_t);
+
+int tx_broadcast_pings(int,struct interface *);
 
 #ifdef __cplusplus
 }
