d79147bf8deba8f47a52bebf3394a8bf6dde0d59
nick black <dank@qemfd.net>
2011-11-22 08:51:58 -0500

    encode most of the ICMP[46] types

--- src/omphalos/icmp.c
+++ src/omphalos/icmp.c
@@ -12,16 +12,55 @@
 #include <omphalos/ethernet.h>
 #include <omphalos/interface.h>
 
+#define COMMON_ICMPV4_LEN 4
 void handle_icmp_packet(omphalos_packet *op,const void *frame,size_t len){
 	const struct icmphdr *icmp = frame;
+	/*const void *dframe;
+	size_t dlen;*/
 
+	// Check length for the mandatory minimum ICMPv4 size...
 	if(len < sizeof(*icmp)){
 		diagnostic(L"%s malformed with %zu",__func__,len);
 		op->malformed = 1;
 		return;
 	}
-	// FIXME
+	// ...but only skip COMMON_ICMPV4_LEN.
+	/*dframe = (const char *)frame + COMMON_ICMPV4_LEN;
+	dlen = len - COMMON_ICMPV4_LEN;*/
+	switch(icmp->type){
+		case ICMP_ECHOREPLY:
+			break;
+		case ICMP_DEST_UNREACH:
+			break;
+		case ICMP_SOURCE_QUENCH:
+			break;
+		case ICMP_REDIRECT:
+			break;
+		case ICMP_ECHO:
+			break;
+		case ICMP_TIME_EXCEEDED:
+			break;
+		case ICMP_PARAMETERPROB:
+			break;
+		case ICMP_TIMESTAMP:
+			break;
+		case ICMP_TIMESTAMPREPLY:
+			break;
+		case ICMP_INFO_REQUEST:
+			break;
+		case ICMP_INFO_REPLY:
+			break;
+		case ICMP_ADDRESS:
+			break;
+		case ICMP_ADDRESSREPLY:
+			break;
+		default:
+			diagnostic(L"Unknown ICMPv4 type: %u",icmp->type);
+			op->noproto = 1;
+			break;
+	}
 }
+#undef COMMON_ICMPV4_LEN
 
 // The icmp_hdr structure includes the second 32-bit word from the header,
 // though this is different among different ICMPv6 types. Pass it along.
@@ -40,18 +79,37 @@ void handle_icmp6_packet(omphalos_packet *op,const void *frame,size_t len){
 	// ...but only skip COMMON_ICMPV6_LEN.
 	dframe = (const char *)frame + COMMON_ICMPV6_LEN;
 	dlen = len - COMMON_ICMPV6_LEN;
+	// Types 1--127 are errors; 128--255 are informational
 	switch(icmp->icmp6_type){
-		case ND_ROUTER_ADVERT:
-			handle_nd_routerad(op,dframe,dlen);
+		case ICMP6_DST_UNREACH: // FIXME
 			break;
-		case ND_NEIGHBOR_ADVERT:
-			handle_nd_neighad(op,dframe,dlen);
+		case ICMP6_PACKET_TOO_BIG: // FIXME
+			break;
+		case ICMP6_TIME_EXCEEDED: // FIXME
+			break;
+		case ICMP6_PARAM_PROB: // FIXME
+			break;
+		case ICMP6_ECHO_REQUEST: // FIXME
+			break;
+		case ICMP6_ECHO_REPLY: // FIXME
+			break;
+		case MLD_LISTENER_QUERY:
+			break;
+		case MLD_LISTENER_REPORT:
+			break;
+		case MLD_LISTENER_REDUCTION:
 			break;
 		case ND_ROUTER_SOLICIT:
 			handle_nd_routersol(op,dframe,dlen);
 			break;
+		case ND_ROUTER_ADVERT:
+			handle_nd_routerad(op,dframe,dlen);
+			break;
 		case ND_NEIGHBOR_SOLICIT:
 			handle_nd_neighsol(op,dframe,dlen);
+			break;
+		case ND_NEIGHBOR_ADVERT:
+			handle_nd_neighad(op,dframe,dlen);
 			break;
 		case ND_REDIRECT:
 			handle_nd_redirect(op,dframe,dlen);
