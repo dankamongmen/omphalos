2e59970d0ec28a1dd4148f93d2aa62cf92dfa47b
nick black <dank@qemfd.net>
2011-12-09 11:57:33 -0500

    start breaking down NetBIOS name service packets

--- src/omphalos/netbios.c
+++ src/omphalos/netbios.c
@@ -12,8 +12,42 @@
 #include <omphalos/omphalos.h>
 #include <omphalos/interface.h>
 
-// Returns 1 for a valid NetBIOS response, -1 for a valid NetBIOS query, 0 otherwise
-int handle_netbios_packet(omphalos_packet *op,const void *frame,size_t len){
-	assert(op && frame && len);
+/* flags field
+struct {
+	unsigned response: 1;
+	unsigned opcode: 4;
+	unsigned unused1: 1;
+	unsigned trunc: 1;
+	unsigned recurse: 1;
+	unsigned unused2: 3;
+	unsigned bcast: 1;
+	unsigned unused3: 4;
+} __attribute__ ((packed));*/
+
+typedef struct smbnshdr {
+	uint16_t tid;
+	uint16_t flags;
+	uint16_t qc,ac,addc,authc;
+} __attribute__ ((packed)) smbnshdr;
+
+#define OPCODE_MASK 0x7800
+
+#define OPCODE_REGISTRATION 0x2800 // (5)
+
+int handle_netbios_ns_packet(omphalos_packet *op,const void *frame,size_t len){
+	const smbnshdr *ns = frame;
+	uint16_t f;
+
+	if(len < sizeof(*ns)){
+		diagnostic("%s NetBIOS NS too small (%zu) on %s",__func__,len,op->i->name);
+		op->malformed = 1;
+		return -1;
+	}
+	f = ntohs(ns->flags);
+	switch(f & OPCODE_MASK){
+		case OPCODE_REGISTRATION:
+			// FIXME extract the name and offer it to resolver
+			break;
+	}
 	return 0;
 }
