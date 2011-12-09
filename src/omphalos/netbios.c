fea288ad882cc2c3c7d210fd323287d5b0700e16
nick black <dank@qemfd.net>
2011-12-09 18:23:14 -0500

    set up for decoding NetBIOS First Level Encoding

--- src/omphalos/netbios.c
+++ src/omphalos/netbios.c
@@ -34,6 +34,53 @@ typedef struct smbnshdr {
 
 #define OPCODE_REGISTRATION 0x2800 // (5)
 
+char *first_level_decode(const char *enc,size_t len){
+	size_t blen = 0,off = 0,slen;
+	unsigned odd = 0;
+	char *ret = NULL;
+
+	if(len == 0){
+		diagnostic("%s truncated First-Level Encoding",__func__);
+		return NULL;
+	}
+	slen = *enc;
+	if(slen + 2 > len){
+		diagnostic("%s short First-Level Encoding (want %zu)",__func__,slen);
+		return NULL;
+	}
+	--len;
+	++enc;
+	while(len && *enc >= 'A' && *enc <= 'P'){
+		if((off + 1) >= blen){
+			char *tmp;
+
+			if((tmp = realloc(ret,blen + 16)) == NULL){
+				free(ret);
+				return NULL;
+			}
+			ret = tmp;
+			blen += 16;
+		}
+		--len;
+		++enc;
+		// FIXME fill it in with decode
+	}
+	if(len == 0){
+		diagnostic("%s truncated First-Level Encoding",__func__);
+		return NULL;
+	}
+	if(odd){
+		diagnostic("%s invalid First-Level Encoding",__func__);
+		return NULL;
+	}
+	if(ret == NULL){
+		diagnostic("%s empty First-Level Encoding",__func__);
+		return NULL;
+	}
+	ret[off] = '\0';
+	return ret;
+}
+
 int handle_netbios_ns_packet(omphalos_packet *op,const void *frame,size_t len){
 	const smbnshdr *ns = frame;
 	uint16_t f;
@@ -45,9 +92,16 @@ int handle_netbios_ns_packet(omphalos_packet *op,const void *frame,size_t len){
 	}
 	f = ntohs(ns->flags);
 	switch(f & OPCODE_MASK){
-		case OPCODE_REGISTRATION:
-			// FIXME extract the name and offer it to resolver
+		case OPCODE_REGISTRATION:{
+			char *name;
+
+			if((name = first_level_decode((const char *)ns + sizeof(*ns),
+							len - sizeof(*ns))) == NULL){
+				return -1;
+			}
+			diagnostic("NetBIOS name: [%s]",name); // FIXME
 			break;
+		}
 	}
 	return 0;
 }
