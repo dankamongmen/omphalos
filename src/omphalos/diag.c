230e71c486778a40ff0196336ff2cfd5b06afe20
nick black <dank@qemfd.net>
2011-12-14 06:10:29 -0500

    introduce get_logs() to access the log ringbuffer. we're not populating it yet, though

--- src/omphalos/diag.c
+++ src/omphalos/diag.c
@@ -1,7 +1,15 @@
 #include <assert.h>
 #include <stdarg.h>
 #include <omphalos/diag.h>
+#include <omphalos/util.h>
 #include <omphalos/omphalos.h>
+
+#define LOG_RINGBUF_SIZE 1024
+
+static unsigned rbstart;
+static char *logs[LOG_RINGBUF_SIZE];
+static pthread_mutex_t loglock = PTHREAD_MUTEX_INITIALIZER;
+
 
 void diagnostic(const char *fmt,...){
 	const omphalos_ctx *octx = get_octx();
@@ -10,4 +18,42 @@ void diagnostic(const char *fmt,...){
 	va_start(va,fmt);
 	octx->iface.vdiagnostic(fmt,va);
 	va_end(va);
+}
+
+char *get_logs(unsigned n,int sep){
+	unsigned rb,left;
+	size_t len = 1;
+	char *l = NULL;
+
+	left = n;
+	Pthread_mutex_lock(&loglock);
+	rb = rbstart;
+	while(logs[rb]){
+		size_t nlen;
+		char *tmp;
+
+		if(n && !left--){
+			break; // got all requested
+		}
+		// FIXME concatenate
+		nlen = len + 1;
+		if((tmp = realloc(l,nlen)) == NULL){
+			free(l);
+			Pthread_mutex_unlock(&loglock);
+			diagnostic("%s couldn't allocate %zu bytes",__func__,nlen);
+			return NULL;
+		}
+		l = tmp;
+		len = nlen;
+		l[len - 2] = sep;
+		if(++rb == sizeof(logs) / sizeof(*logs)){
+			rb = 0;
+		}
+		if(rb == rbstart){
+			break;
+		}
+	}
+	Pthread_mutex_unlock(&loglock);
+	l[len - 1] = '\0';
+	return l;
 }
--- src/omphalos/diag.h
+++ src/omphalos/diag.h
@@ -9,6 +9,11 @@ extern "C" {
 // lookup on a TSD (omphalos_ctx_key).
 void diagnostic(const char *,...) __attribute__ ((format (printf,1,2)));
 
+// Get the last n diagnostics. If n is 0 or larger than the number of available
+// diagnostics, all available diagnostics will be returned. Each diagnostic
+// will be terminated with the provided separator.
+char *get_logs(unsigned,int);
+
 #ifdef __cplusplus
 }
 #endif
