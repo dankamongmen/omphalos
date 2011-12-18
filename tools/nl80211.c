7b355523b3392b8ebc782921de7591db8c9b5a2a
nick black <dank@qemfd.net>
2011-12-18 14:19:13 -0500

    nl80211 tester

--- tools/nl80211.c
+++ tools/nl80211.c
@@ -4,14 +4,25 @@
 #include <omphalos/nl80211.h>
 #include <omphalos/omphalos.h>
 
+static void
+diag(const char *fmt,va_list va){
+	fprintf(stderr,fmt,va);
+}
+
 int main(void){
+	omphalos_ctx ctx = {
+		.iface = {
+			.vdiagnostic = diag,
+		},
+	};
 	int ret;
 
-	pthread_setspecific(omphalos_ctx_key,NULL);
+	pthread_setspecific(omphalos_ctx_key,&ctx);
 	if(open_nl80211()){
-		fprintf(stderr,"Failure opening nl80211\n");
+		fprintf(stderr,"Failure opening cfg80211\n");
 		ret = -1;
 	}else{
+		fprintf(stdout,"Successfully opened cfg80211\n");
 		ret |= close_nl80211();
 	}
 	exit(ret ? EXIT_FAILURE : EXIT_SUCCESS);
