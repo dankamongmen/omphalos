69c01fe4c8edc035afa256ce38a417f75357dc7e
nick black <dank@qemfd.net>
2011-12-18 12:54:29 -0500

    add iface_uses_offloading() for bug 472

--- src/omphalos/ethtool.h
+++ src/omphalos/ethtool.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#include <omphalos/interface.h>
+
 #define RX_CSUM_OFFLOAD		0x0001
 #define TX_CSUM_OFFLOAD		0x0002
 #define ETH_SCATTER_GATHER	0x0004
@@ -18,14 +20,22 @@ extern "C" {
 #define NTUPLE_FILTERS		0x0400
 #define RXPATH_HASH		0x0800
 
-struct interface;
 struct ethtool_cmd;
 struct ethtool_drvinfo;
 
 int iface_driver_info(const char *,struct ethtool_drvinfo *);
 int iface_ethtool_info(const char *,struct ethtool_cmd *);
 int iface_offload_info(const char *,unsigned *,unsigned *);
-int iface_offloaded_p(const struct interface *,unsigned);
+int iface_offloaded_p(const interface *,unsigned);
+
+// Check for LRO/GRO/GSO use on the interface -- if they're active, we want to
+// use RX frames larger than the MTU.
+static inline int
+iface_uses_offloading(const interface *i){
+	return iface_offloaded_p(i,GENRX_OFFLOAD) ||
+		iface_offloaded_p(i,GEN_SEG_OFFLOAD) ||
+		iface_offloaded_p(i,TCP_SEG_OFFLOAD);
+}
 
 #ifdef __cplusplus
 }
