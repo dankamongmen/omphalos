9da783c9a03370fee2937b846f5cc1853c01345d
nick black <dank@qemfd.net>
2011-12-21 18:50:58 -0500

    need track a few more things in nl80211; fill out nl80211_info definition

--- src/omphalos/nl80211.h
+++ src/omphalos/nl80211.h
@@ -12,7 +12,12 @@ struct interface;
 #define MAX_WIRELESS_CHANNEL		999
 
 typedef struct nl80211_info {
-	uintmax_t freqs[MAX_WIRELESS_CHANNEL];
+	uintmax_t freqs[MAX_WIRELESS_CHANNEL];	// Set if channel is defined
+	float dBm[MAX_WIRELESS_CHANNEL];	// Set if enabled
+	int chan;				// Current channel or -1
+	uintmax_t freq;				// Current freq or 0
+	unsigned mode;				// Current mode
+	unsigned bitrate;			// Current bitrate or 0
 } nl80211_info;
 
 int open_nl80211(void);
