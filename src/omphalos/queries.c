21ef142eaff34499831955b5c1a2fd312a394122
nick black <dank@qemfd.net>
2011-12-25 21:05:28 -0500

    workaround for bug 474 -- don't do broadbase queries on loopbacks

--- src/omphalos/queries.c
+++ src/omphalos/queries.c
@@ -1,3 +1,4 @@
+#include <net/if_arp.h>
 #include <omphalos/icmp.h>
 #include <omphalos/mdns.h>
 #include <omphalos/lltd.h>
@@ -8,11 +9,13 @@
 int query_network(int family,interface *i,const void *saddr){
 	int r = 0;
 
-	r |= initiate_lltd(family,i,saddr);
-	r |= tx_broadcast_pings(family,i,saddr);
-	r |= mdns_sd_enumerate(family,i,saddr);
-	r |= mdns_stdsd_probe(family,i,saddr);
-	r |= ssdp_msearch(family,i,saddr);
+	if(i->arptype != ARPHRD_LOOPBACK){
+		r |= initiate_lltd(family,i,saddr);
+		r |= tx_broadcast_pings(family,i,saddr);
+		r |= mdns_sd_enumerate(family,i,saddr);
+		r |= mdns_stdsd_probe(family,i,saddr);
+		r |= ssdp_msearch(family,i,saddr);
+	}
 	return r;
 }
 
