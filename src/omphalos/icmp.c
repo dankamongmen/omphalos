529dcc7b696d24120db992d35a0f30b5be4b059a
nick black <dank@qemfd.net>
2011-12-30 22:00:09 -0500

    kill loops in icmp broadcasts

--- src/omphalos/icmp.c
+++ src/omphalos/icmp.c
@@ -133,122 +133,88 @@ void handle_icmp6_packet(omphalos_packet *op,const void *frame,size_t len){
 #define PING4_PAYLOAD_LEN 20
 
 // Always goes to ff02::2 (ALL-HOSTS), from each source address.
-int tx_ipv4_bcast_pings(interface *i,const void *saddr){
-	const struct ip4route *i4;
-	int ret = 0;
+int tx_ipv4_bcast_pings(interface *i,const uint32_t *saddr){
+	struct tpacket_hdr *thdr;
+	struct icmphdr *icmp;
+	struct iphdr *ip;
+	size_t flen,tlen;
+	uint32_t net;
+	void *frame;
+	int r;
 
-	for(i4 = i->ip4r ; i4 ; i4 = i4->next){
-		struct tpacket_hdr *thdr;
-		struct icmphdr *icmp;
-		struct iphdr *ip;
-		size_t flen,tlen;
-		uint32_t net;
-		void *frame;
-		int r;
-
-		if(!(i4->addrs & ROUTE_HAS_SRC)){
-			continue; // not cause for an error
-		}
-		if(memcmp(&i4->src,saddr,sizeof(i4->src))){
-			continue;
-		}
-		if((frame = get_tx_frame(i,&flen)) == NULL){
-			ret = -1;
-			continue;
-		}
-		thdr = frame;
-		tlen = thdr->tp_mac;
-		if((r = prep_eth_bcast((char *)frame + tlen,flen - tlen,i,ETH_P_IP)) < 0){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		tlen += r;
-		ip = (struct iphdr *)((char *)frame + tlen);
-		net = htonl(0xfffffffful); // FIXME get bcast for route
-		if((r = prep_ipv4_header(ip,flen - tlen,i4->src,net,IPPROTO_ICMP)) < 0){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		tlen += r;
-		if(flen - tlen < sizeof(*icmp) + PING4_PAYLOAD_LEN){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		icmp = (struct icmphdr *)((char *)frame + tlen);
-		icmp->type = ICMP_ECHO;
-		icmp->code = 0;
-		tlen += sizeof(*icmp) + PING4_PAYLOAD_LEN;
-		thdr->tp_len = tlen;
-		ip->tot_len = htons((const char *)icmp - (const char *)ip + sizeof(*icmp) + PING4_PAYLOAD_LEN);
-		icmp->checksum = 0;
-		icmp->checksum = icmp4_csum(icmp,sizeof(*icmp));
-		ip->check = ipv4_csum(ip);
-		send_tx_frame(i,frame); // FIXME get return value...
-		break; // we're done
+	if((frame = get_tx_frame(i,&flen)) == NULL){
+		return -1;
 	}
-	return ret;
+	thdr = frame;
+	tlen = thdr->tp_mac;
+	if((r = prep_eth_bcast((char *)frame + tlen,flen - tlen,i,ETH_P_IP)) < 0){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	tlen += r;
+	ip = (struct iphdr *)((char *)frame + tlen);
+	net = htonl(0xfffffffful); // FIXME get bcast for route
+	if((r = prep_ipv4_header(ip,flen - tlen,*saddr,net,IPPROTO_ICMP)) < 0){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	tlen += r;
+	if(flen - tlen < sizeof(*icmp) + PING4_PAYLOAD_LEN){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	icmp = (struct icmphdr *)((char *)frame + tlen);
+	icmp->type = ICMP_ECHO;
+	icmp->code = 0;
+	tlen += sizeof(*icmp) + PING4_PAYLOAD_LEN;
+	thdr->tp_len = tlen;
+	ip->tot_len = htons((const char *)icmp - (const char *)ip + sizeof(*icmp) + PING4_PAYLOAD_LEN);
+	icmp->checksum = 0;
+	icmp->checksum = icmp4_csum(icmp,sizeof(*icmp));
+	ip->check = ipv4_csum(ip);
+	return send_tx_frame(i,frame);
 }
 
 // Always goes to ff02::2 (ALL-HOSTS), from each source address.
 int tx_ipv6_bcast_pings(interface *i,const void *saddr){
-	const struct ip6route *i6;
-	int ret = 0;
+	const unsigned char hw[ETH_ALEN] = { 0x33, 0x33, 0x00, 0x00, 0x00, 0x01 };
+	uint128_t net = { htonl(0xff020000ul), htonl(0x0ul),
+				htonl(0x0ul), htonl(0x1ul) };
+	struct tpacket_hdr *thdr;
+	struct icmp6_hdr *icmp;
+	struct ip6_hdr *ip;
+	size_t flen,tlen;
+	void *frame;
+	int r;
 
-	for(i6 = i->ip6r ; i6 ; i6 = i6->next){
-		const unsigned char hw[ETH_ALEN] = { 0x33, 0x33, 0x00, 0x00, 0x00, 0x01 };
-		uint128_t net = { htonl(0xff020000ul), htonl(0x0ul),
-					htonl(0x0ul), htonl(0x1ul) };
-		struct tpacket_hdr *thdr;
-		struct icmp6_hdr *icmp;
-		struct ip6_hdr *ip;
-		size_t flen,tlen;
-		void *frame;
-		int r;
-
-		if(!(i6->addrs & ROUTE_HAS_SRC)){
-			continue; // not cause for an error
-		}
-		if(!equal128(i6->src,saddr)){
-			continue;
-		}
-		if((frame = get_tx_frame(i,&flen)) == NULL){
-			ret = -1;
-			continue;
-		}
-		thdr = frame;
-		tlen = thdr->tp_mac;
-		if((r = prep_eth_header((char *)frame + tlen,flen - tlen,i,
-						hw,ETH_P_IPV6)) < 0){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		tlen += r;
-		ip = (struct ip6_hdr *)((char *)frame + tlen);
-		if((r = prep_ipv6_header(ip,flen - tlen,i6->src,net,IPPROTO_ICMP6)) < 0){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		tlen += r;
-		if(flen - tlen < sizeof(*icmp)){
-			abort_tx_frame(i,frame);
-			ret = -1;
-			continue;
-		}
-		icmp = (struct icmp6_hdr *)((char *)frame + tlen);
-		icmp->icmp6_type = ICMP6_ECHO_REQUEST;
-		icmp->icmp6_code = 0;
-		tlen += sizeof(*icmp);
-		thdr->tp_len = tlen;
-		ip->ip6_ctlun.ip6_un1.ip6_un1_plen = htons(thdr->tp_len -
-			((const char *)icmp - (const char *)frame));
-		icmp->icmp6_cksum = icmp6_csum(ip);
-		send_tx_frame(i,frame); // FIXME get return value...
-		break; // we're done
+	if((frame = get_tx_frame(i,&flen)) == NULL){
+		return -1;
 	}
-	return ret;
+	thdr = frame;
+	tlen = thdr->tp_mac;
+	if((r = prep_eth_header((char *)frame + tlen,flen - tlen,i,
+					hw,ETH_P_IPV6)) < 0){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	tlen += r;
+	ip = (struct ip6_hdr *)((char *)frame + tlen);
+	if((r = prep_ipv6_header(ip,flen - tlen,saddr,net,IPPROTO_ICMP6)) < 0){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	tlen += r;
+	if(flen - tlen < sizeof(*icmp)){
+		abort_tx_frame(i,frame);
+		return -1;
+	}
+	icmp = (struct icmp6_hdr *)((char *)frame + tlen);
+	icmp->icmp6_type = ICMP6_ECHO_REQUEST;
+	icmp->icmp6_code = 0;
+	tlen += sizeof(*icmp);
+	thdr->tp_len = tlen;
+	ip->ip6_ctlun.ip6_un1.ip6_un1_plen = htons(thdr->tp_len -
+		((const char *)icmp - (const char *)frame));
+	icmp->icmp6_cksum = icmp6_csum(ip);
+	return send_tx_frame(i,frame);
 }
--- src/omphalos/icmp.h
+++ src/omphalos/icmp.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifndef IPPROTO_ICMP6
 #define IPPROTO_ICMP6	58
@@ -18,7 +19,7 @@ void handle_icmp_packet(struct omphalos_packet *,const void *,size_t);
 
 void handle_icmp6_packet(struct omphalos_packet *,const void *,size_t);
 
-int tx_ipv4_bcast_pings(struct interface *,const void *);
+int tx_ipv4_bcast_pings(struct interface *,const uint32_t *);
 int tx_ipv6_bcast_pings(struct interface *,const void *);
 
 #ifdef __cplusplus
