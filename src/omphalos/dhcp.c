1416e7ca63cfa930dd9e555b785d21b624bbfdf4
nick black <dank@qemfd.net>
2012-03-03 01:41:41 -0500

    fill in dhcp probe udp headers with prep_udp[46]()

--- src/omphalos/dhcp.c
+++ src/omphalos/dhcp.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <omphalos/ip.h>
 #include <omphalos/tx.h>
+#include <omphalos/udp.h>
 #include <omphalos/dhcp.h>
 #include <omphalos/omphalos.h>
 #include <omphalos/ethernet.h>
@@ -15,6 +16,8 @@ int handle_dhcp6_packet(omphalos_packet *op,const void *frame,size_t fsize){
 	assert(op && frame && fsize);
 	return 1; // FIXME
 }
+
+#define DHCP4_REQ_LEN 44
 
 int dhcp4_probe(interface *i,const uint32_t *saddr){
 	size_t fsize,off;
@@ -35,6 +38,11 @@ int dhcp4_probe(interface *i,const uint32_t *saddr){
 	}
 	fsize -= r;
 	off += r;
+	if((r = prep_udp4((char *)frame + off,fsize,DHCP_UDP_PORT,BOOTP_UDP_PORT,DHCP4_REQ_LEN)) < 0){
+		goto err;
+	}
+	fsize -= r;
+	off += r;
 	// FIXME
 	send_tx_frame(i,frame);
 	return 0;
@@ -44,6 +52,7 @@ err:
 	return -1;
 }
 
+#define DHCP6_REQ_LEN 8
 int dhcp6_probe(interface *i,const uint128_t saddr){
 	uint128_t daddr = DHCPV6_RELAYSSERVERS;
 	size_t fsize,off;
@@ -60,6 +69,11 @@ int dhcp6_probe(interface *i,const uint128_t saddr){
 	fsize -= r;
 	off += r;
 	if((r = prep_ipv6_header((char *)frame + off,fsize,saddr,daddr,IPPROTO_UDP)) < 0){
+		goto err;
+	}
+	fsize -= r;
+	off += r;
+	if((r = prep_udp6((char *)frame + off,fsize,DHCP6CLI_UDP_PORT,DHCP6SRV_UDP_PORT,DHCP6_REQ_LEN)) < 0){
 		goto err;
 	}
 	fsize -= r;
