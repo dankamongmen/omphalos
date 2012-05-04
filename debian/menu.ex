b51d8284edea4fd7501521ee401bd3fb55706a78
nick black <dank@qemfd.net>
2012-05-03 22:16:44 -0400

    set up a watch file remove example files update email address in control file

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(omphalos):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="omphalos" command="/usr/bin/omphalos"
--- debian/omphalos.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the omphalos package
-#
-0 4	* * *	root	[ -x /usr/bin/omphalos_maintenance ] && /usr/bin/omphalos_maintenance
