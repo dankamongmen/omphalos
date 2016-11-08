f461227ca1533688a4fad66d92d0321bf021f7cb
nick black <dankamongmen@gmail.com>
2016-11-08 16:02:41 -0500

    do basic length check against ESP/AH headers rather than possibly asserting out

--- src/omphalos/ipsec.c
+++ src/omphalos/ipsec.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <linux/ip.h>
 #include <omphalos/diag.h>
 #include <omphalos/ipsec.h>
 #include <omphalos/omphalos.h>
@@ -6,10 +7,21 @@
 
 // Encapsulated Security Payload
 void handle_esp_packet(omphalos_packet *op,const void *frame,size_t len){
-	assert(op && frame && len); // FIXME
+	const struct ip_esp_hdr *esp = frame;
+	if(len < sizeof(*esp)){
+		diagnostic("%s malformed with %zu",__func__,len);
+		op->malformed = 1;
+		return;
+	}
+	// FIXME
 }
 
 // Authentication Header
 void handle_ah_packet(omphalos_packet *op,const void *frame,size_t len){
-	assert(op && frame && len); // FIXME
+	const struct ip_auth_hdr *ah = frame;
+	if(len < sizeof(*ah)){
+		diagnostic("%s malformed with %zu",__func__,len);
+		op->malformed = 1;
+		return;
+	}
 }
