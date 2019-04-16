56abac2ed0da851f19f7420d915ddeac8eddd481
nick black <dankamongmen@gmail.com>
2019-04-16 18:23:51 -0400

    Nothing I love more than fucking with autoconf (#2)

--- src/omphalos/sysfs.c
+++ src/omphalos/sysfs.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
+// libsysfs implementation
+#ifdef HAVE_SYSFS
+#include <sysfs/libsysfs.h>
+#else
 #include <libsysfs.h>
+#endif
 #include <omphalos/usb.h>
 #include <omphalos/pci.h>
 #include <omphalos/sysfs.h>
