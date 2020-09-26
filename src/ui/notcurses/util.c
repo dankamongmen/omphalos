ba4e0b0409e9531717ad0e43085a059137042fb4
nick black <dankamongmen@gmail.com>
2020-09-26 19:47:17 -0400

    the conversion never ends

--- src/ui/notcurses/util.c
+++ src/ui/notcurses/util.c
@@ -11,11 +11,11 @@ int bevel_bottom(struct ncplane *w){
 	};
 	int rows,cols;
 
-	getmaxyx(w,rows,cols);
-	assert(rows && cols);
+	ncplane_dim_yx(w, &rows, &cols);
+	assert(rows > 0 && cols > 0);
 	//assert(mvwadd_wch(w,rows - 1,0,&bchr[0]) != ERR);
-	assert(mvwhline(w,rows - 1,2,ACS_HLINE,cols - 2) != ERR);
-	assert(mvwins_wch(w,rows - 1,cols - 1,&bchr[1]) != ERR);
+	mvwhline(w, rows - 1, 2, ACS_HLINE, cols - 2);
+	mvwins_wch(w, rows - 1, cols - 1, &bchr[1]);
 	return 0;
 }
 
@@ -41,23 +41,23 @@ int bevel(struct ncplane *w){
 		{ .attr = 0, .chars = L"╰", },
 		{ .attr = 0, .chars = L"╯", },
 	};
-	int rows,cols;
+	int rows, cols;
 
-	getmaxyx(w,rows,cols);
-	assert(rows && cols);
+	ncplane_dim_yx(w, &rows, &cols);
+	assert(rows > 0 && cols > 0);
 	// called as one expects: 'mvwadd_wch(w,rows - 1,cols - 1,&bchr[3]);'
 	// we get ERR returned. this is known behavior: fuck ncurses. instead,
 	// we use mvwins_wch, which doesn't update the cursor position.
 	// see http://lists.gnu.org/archive/html/bug-ncurses/2007-09/msg00001.html
-	assert(mvwadd_wch(w,0,0,&bchr[0]) != ERR);
-	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
-	assert(mvwins_wch(w,0,cols - 1,&bchr[1]) != ERR);
+	mvwadd_wch(w, 0, 0, &bchr[0]);
+	whline(w, ACS_HLINE, cols - 2);
+	mvwins_wch(w, 0, cols - 1, &bchr[1]);
 	if(rows > 1){
-		assert(mvwvline(w,1,cols - 1,ACS_VLINE,rows - 1) != ERR);
-		assert(mvwvline(w,1,0,ACS_VLINE,rows - 1) != ERR);
+		mvwvline(w, 1, cols - 1, ACS_VLINE, rows - 1);
+		mvwvline(w, 1, 0, ACS_VLINE, rows - 1);
 	}
-	assert(mvwadd_wch(w,rows - 1,0,&bchr[2]) != ERR);
-	assert(whline(w,ACS_HLINE,cols - 2) != ERR);
-	assert(mvwins_wch(w,rows - 1,cols - 1,&bchr[3]) != ERR);
+	mvwadd_wch(w, rows - 1, 0, &bchr[2]);
+	whline(w, ACS_HLINE, cols - 2);
+	mvwins_wch(w, rows - 1, cols - 1, &bchr[3]);
 	return 0;
 }
